9034fc1069e0686e2f91b8306659bf5d68b25204
ThomasCardin <tomcardin@outlook.com>
2024-03-09 14:30:06 -0500

    create uuid when an account is created

--- pkg/account/account.go
+++ pkg/account/account.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ClubCedille/hackqc2024/pkg/database"
 	"github.com/ostafen/clover/v2"
 	"github.com/ostafen/clover/v2/document"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Account struct {
@@ -15,6 +16,7 @@ type Account struct {
 }
 
 func CreateAccount(conn *clover.DB, account Account) error {
+	account.Id = uuid.NewV4().String()
 	accountDoc := document.NewDocumentOf(account)
 	err := conn.Insert(database.AccountCollection, accountDoc)
 	if err != nil {
